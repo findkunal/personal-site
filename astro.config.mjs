11769ef592d8d7828bfed4ddc19b972424a05bae
Kunal <findkunal@gmail.com>
2026-04-29 20:58:45 -0400

    Set site URL to Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourdomain.com',
+  site: 'https://personal-site-fawn-kappa.vercel.app',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'gu'],
